0c158d70c6188a943d9216cb955cc8b8f3f9dac6
Nick Whaley <nicholasrwhaley@gmail.com>
2019-11-11 15:35:56 -0700

    begin working with fs to write info

--- movielibrary/src/App.js
+++ movielibrary/src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import MovieEntry from './components/movieEntry.jsx'
 import movieList from './components/movieList.js'
 
+const fs = require('fs-extra')
 
+const greeting = 'hi file'
 const appStyle = {
 
   backgroundColor: 'black',
@@ -22,6 +24,7 @@ class App extends React.Component {
     }
 
     this.selectRandom = this.selectRandom.bind(this)
+    this.fileWrite = this.fileWrite.bind(this)
   }
 
 
@@ -30,6 +33,13 @@ class App extends React.Component {
       randomMovie : this.state.movies[Math.floor(Math.random() * Math.floor(this.state.movies.length - 1))]
    }))
 }
+
+  componentWillMount(){
+    fs.write('message.txt', greeting, 'utf8', (err) => {
+      if (err) throw err;
+      console.log('The file has been saved!');
+    });
+  }
 
 
   render(){
@@ -43,6 +53,7 @@ class App extends React.Component {
            {this.state.randomMovie.genre}</div>
 
         </div>
+        <button onClick={this.fileWrite}>Save</button>
       {this.state.movies.map((movie) => (
          <p>
          <MovieEntry name={movie.name} year={movie.year} genre={movie.genre}/>
